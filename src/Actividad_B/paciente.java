6f13af1d826a69413efd7a85b8b95a7ed575ed04
matias <elhierrosoluciones@gmail.com>
2022-04-10 11:58:21 +0100

    actualizacion 1

---
+++ src/Actividad_B/paciente.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package Actividad_B;
+
+
+import java.util.GregorianCalendar;
+
+/**
+ * @author Matias Machin
+ *
+ */
+public class paciente {
+	
+	// atributos
+	
+	private int nrohistoria;
+	private String nombre;
+	private String apellidos;
+	private GregorianCalendar  fechanac;
+	private String dni;
+	private String sexo;
+	private String direccion;
+	private String telefono;
+	private String cip;  // codigo de identificacion del paciente
+	private int ss; // nro de seguridad social
+	
+	
+	
+	
+
+}
