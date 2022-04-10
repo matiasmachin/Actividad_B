ba88899e4eb66eb6cb4804eacc22d77c312c1883
matias <elhierrosoluciones@gmail.com>
2022-04-10 12:12:09 +0100

    actualizacion

--- src/Actividad_B/paciente.java
+++ src/Actividad_B/paciente.java
@@ -23,12 +23,35 @@ public class paciente {
 	private String direccion;
 	private String telefono;
 	private String cip;  // codigo de identificacion del paciente
-	
-	
+	private int ss; // nro de seguridad social
+
 	// constructores
 	
-	
-	private int ss; // nro de seguridad social
+	/**
+	 * @param nrohistoria
+	 * @param nombre
+	 * @param apellidos
+	 * @param fechanac
+	 * @param dni
+	 * @param sexo
+	 * @param direccion
+	 * @param telefono
+	 * @param cip
+	 * @param ss
+	 */
+	public paciente(int nrohistoria, String nombre, String apellidos, GregorianCalendar fechanac, String dni,
+			String sexo, String direccion, String telefono, String cip, int ss) {
+		this.nrohistoria = nrohistoria;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.fechanac = fechanac;
+		this.dni = dni;
+		this.sexo = sexo;
+		this.direccion = direccion;
+		this.telefono = telefono;
+		this.cip = cip;
+		this.ss = ss;
+	}
 	
 	
 	
